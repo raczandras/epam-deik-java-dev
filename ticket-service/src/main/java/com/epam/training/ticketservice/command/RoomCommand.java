ca2cea807fb9b211e6167c98f977893b9c6ec0e3
raczandras <raczandras0204@gmail.com>
2021-05-02 19:31:35 +0200

    The room commands work now

--- ticket-service/src/main/java/com/epam/training/ticketservice/command/RoomCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/command/RoomCommand.java
@@ -1,36 +1,70 @@
 package com.epam.training.ticketservice.command;
 
+import com.epam.training.ticketservice.core.room.RoomService;
+import com.epam.training.ticketservice.core.room.exception.RoomAlreadyExistsException;
+import com.epam.training.ticketservice.core.room.exception.RoomNotFoundException;
+import com.epam.training.ticketservice.core.room.model.RoomDto;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
 
+import java.util.List;
+
 @ShellComponent
 public class RoomCommand {
-    //TODO make commands actually work
+    private final RoomService roomService;
 
-    public RoomCommand() {
+    public RoomCommand(RoomService roomService) {
+        this.roomService = roomService;
     }
 
     @ShellMethod(value = "List All Rooms", key = "list rooms")
-    public String[] listRooms() {
-        return "Elso filme;Masodik film;Harmadik film".split(";");
+    public List<RoomDto> listRooms() {
+        return roomService.getRoomList();
     }
 
     @ShellMethodAvailability("isAvailable")
     @ShellMethod(value = "Create a new Room", key = "create room")
     public void createRoom(String name, int rows, int columns) {
+        RoomDto roomDto = RoomDto.builder()
+                .name(name)
+                .rows(rows)
+                .columns(columns)
+                .build();
+        try {
+            roomService.createRoom(roomDto);
+            System.out.println("Room Created: " + roomDto);
+        } catch (RoomAlreadyExistsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @ShellMethodAvailability("isAvailable")
     @ShellMethod(value = "Update a room", key = "update room")
     public void updateRoom(String name, int rows, int columns) {
-            System.out.println("Room Updated: ");
+        RoomDto roomDto = RoomDto.builder()
+                .name(name)
+                .rows(rows)
+                .columns(columns)
+                .build();
+        try {
+            roomService.updateRoom(roomDto);
+            System.out.println("Room Updated: " + roomDto);
+        } catch (RoomNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @ShellMethodAvailability("isAvailable")
     @ShellMethod(value = "Delete room by name", key = "delete room")
     public void deleteRoom(String name) {
+        try {
+            roomService.deleteRoom(name);
+            System.out.printf("Room %s deleted%n", name);
+        } catch (RoomNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private Availability isAvailable() {
