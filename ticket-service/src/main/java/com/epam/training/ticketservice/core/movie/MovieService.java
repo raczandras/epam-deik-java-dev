9616f99906a33927254bdb1db25b6091c2e8500f
raczandras <raczandras0204@gmail.com>
2021-05-02 18:47:48 +0200

    Created the movie service

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/movie/MovieService.java
@@ -0,0 +1,15 @@
+package com.epam.training.ticketservice.core.movie;
+
+import com.epam.training.ticketservice.core.movie.exception.MovieAlreadyExistsException;
+import com.epam.training.ticketservice.core.movie.exception.MovieNotFoundException;
+import com.epam.training.ticketservice.core.movie.model.MovieDto;
+
+import java.util.List;
+
+public interface MovieService {
+
+    List<MovieDto> getMovieList();
+    void createMovie(MovieDto movie) throws MovieAlreadyExistsException;
+    void deleteMovie(String title) throws MovieNotFoundException;
+    void updateMovie(MovieDto movieDto) throws MovieNotFoundException;
+}
