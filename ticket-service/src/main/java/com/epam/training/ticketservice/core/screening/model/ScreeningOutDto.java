60fbf4cf259ad9db810053115f2e3c4d8e2c97d4
raczandras <raczandras0204@gmail.com>
2021-05-02 20:54:50 +0200

    Created the out dto class for listing

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/model/ScreeningOutDto.java
@@ -0,0 +1,23 @@
+package com.epam.training.ticketservice.core.screening.model;
+
+import com.epam.training.ticketservice.core.movie.persistence.entity.MovieEntity;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Generated;
+import lombok.Getter;
+
+@Builder
+@EqualsAndHashCode
+@Getter
+@Generated
+public class ScreeningOutDto {
+
+    private final MovieEntity movie;
+    private final String room;
+    private final String startDate;
+
+    @Override
+    public String toString() {
+        return String.format(movie.getTitle()+" ("+movie.getGenre()+", "+movie.getScreeningTime()+" minutes), screened in room "+room+", at "+startDate);
+    }
+}
