54ac0e30ddccbadd975f5d7482162ee8162ed156
raczandras <raczandras0204@gmail.com>
2021-05-02 18:20:39 +0200

    Created empty user command methods

--- ticket-service/src/main/java/com/epam/training/ticketservice/command/UserCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/command/UserCommand.java
@@ -1,4 +1,27 @@
 package com.epam.training.ticketservice.command;
 
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
 public class UserCommand {
+    //TODO make commands actually work
+    
+    public UserCommand() {
+    }
+
+    @ShellMethod(value = "Login as admin", key = "sign in privileged")
+    public String loginUser(String username, String password) {
+        return "logged in (maybe)";
+    }
+
+    @ShellMethod(value = "Describe current account", key = "describe account")
+    public String describeUser() {
+        return "Signed in with: ";
+    }
+
+    @ShellMethod(value = "Log out from current account", key = "sign out")
+    public String logout() {
+        return "logged out";
+    }
 }
