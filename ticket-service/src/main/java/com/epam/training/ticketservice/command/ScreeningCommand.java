3043f6c677f6cc49d17a2e3a1ecfb34397c455a2
raczandras <raczandras0204@gmail.com>
2021-05-02 20:56:09 +0200

    Made the screening commands work

--- ticket-service/src/main/java/com/epam/training/ticketservice/command/ScreeningCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/command/ScreeningCommand.java
@@ -1,32 +1,65 @@
 package com.epam.training.ticketservice.command;
 
+import com.epam.training.ticketservice.core.movie.exception.MovieNotFoundException;
+import com.epam.training.ticketservice.core.room.exception.RoomNotFoundException;
+import com.epam.training.ticketservice.core.screening.ScreeningService;
+import com.epam.training.ticketservice.core.screening.exception.ScreeningNotFoundException;
+import com.epam.training.ticketservice.core.screening.exception.ScreeningsOverlapException;
+import com.epam.training.ticketservice.core.screening.model.ScreeningDto;
+import com.epam.training.ticketservice.core.screening.model.ScreeningOutDto;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
 
+import java.text.ParseException;
+import java.util.List;
+
 @ShellComponent
 public class ScreeningCommand {
-    //TODO make commands actually work
+    private final ScreeningService screeningService;
 
-    public ScreeningCommand() {
+    public ScreeningCommand(ScreeningService screeningService) {
+        this.screeningService = screeningService;
+    }
+
+    @ShellMethod(value = "List all screenings", key = "list screenings")
+    public List<ScreeningOutDto> listScreenings() {
+        return screeningService.listScreenings();
     }
 
     @ShellMethodAvailability("isAvailable")
     @ShellMethod(value = "Create a new screening", key = "create screening")
-    public void createScreening(String movieTitle, String roomName, String startDate) {
-    }
-
-    @ShellMethod(value = "List all screenings", key = "list screenings")
-    public String[] listScreenings() {
-        return "Elso filme;Masodik film;Harmadik film".split(";");
+    public void createScreening(String movie, String room, String startDate) {
+        ScreeningDto screeningDto = ScreeningDto.builder()
+                .movie(movie)
+                .room(room)
+                .startDate(startDate)
+                .build();
+        try {
+            screeningService.createScreening(screeningDto);
+            System.out.println("Screening created: " + screeningDto);
+        } catch (RoomNotFoundException | MovieNotFoundException
+                | ParseException | ScreeningsOverlapException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @ShellMethodAvailability("isAvailable")
     @ShellMethod(value = "Delete a screening", key = "delete screening")
     public void deleteScreening(String movieTitle, String roomName, String startDate) {
-        System.out.println("Screening deleted: ");
-
+        ScreeningDto screeningDto = ScreeningDto.builder()
+                .movie(movieTitle)
+                .startDate(startDate)
+                .room(roomName)
+                .build();
+        try {
+            screeningService.deleteScreening(screeningDto);
+            System.out.println("Screening deleted: " + screeningDto);
+        } catch (RoomNotFoundException | MovieNotFoundException
+                | ScreeningNotFoundException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private Availability isAvailable() {
