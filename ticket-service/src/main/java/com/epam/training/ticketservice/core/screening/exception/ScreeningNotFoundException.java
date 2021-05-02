d1f517aafd5acd8560ce74097712a10304de314c
raczandras <raczandras0204@gmail.com>
2021-05-02 20:00:12 +0200

    Created the possible exceptions for the screenings

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/exception/ScreeningNotFoundException.java
@@ -0,0 +1,7 @@
+package com.epam.training.ticketservice.core.screening.exception;
+
+public class ScreeningNotFoundException extends Exception {
+    public ScreeningNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/screening/exception/ScreeningsOverlapException.java
@@ -0,0 +1,7 @@
+package com.epam.training.ticketservice.core.screening.exception;
+
+public class ScreeningsOverlapException extends Exception{
+    public ScreeningsOverlapException(String message){
+        super(message);
+    }
+}
