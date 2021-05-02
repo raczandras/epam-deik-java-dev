05e77ea86f6acb3d2ba0272d5f3d8439b6b55f47
raczandras <raczandras0204@gmail.com>
2021-05-02 18:16:51 +0200

    Created empty screening commands

--- ticket-service/src/main/java/com/epam/training/ticketservice/command/ScreeningCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/command/ScreeningCommand.java
@@ -1,4 +1,37 @@
 package com.epam.training.ticketservice.command;
 
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+
+import java.text.ParseException;
+
+@ShellComponent
 public class ScreeningCommand {
+    //TODO make commands actually work
+
+    public ScreeningCommand() {
+    }
+
+    @ShellMethodAvailability("isAvailable")
+    @ShellMethod(value = "Create a new screening", key = "create screening")
+    public void createScreening(String movieTitle, String roomName, String startDate) {
+    }
+
+    @ShellMethod(value = "List all screenings", key = "list screenings")
+    public String[] listScreenings() {
+        return "Elso filme;Masodik film;Harmadik film".split(";");
+    }
+
+    @ShellMethodAvailability("isAvailable")
+    @ShellMethod(value = "Delete a screening", key = "delete screening")
+    public void deleteScreening(String movieTitle, String roomName, String startDate) {
+        System.out.println("Screening deleted: ");
+
+    }
+
+    private Availability isAvailable() {
+        return Availability.available();
+    }
 }
