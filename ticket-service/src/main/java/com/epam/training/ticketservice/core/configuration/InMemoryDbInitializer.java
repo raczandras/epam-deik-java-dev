31c59499cca774ddd68bd52c53569e00ca3d8518
raczandras <raczandras0204@gmail.com>
2021-05-03 19:22:52 +0200

    Created the default admin user in the db

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/configuration/InMemoryDbInitializer.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/configuration/InMemoryDbInitializer.java
@@ -1,4 +1,23 @@
 package com.epam.training.ticketservice.core.configuration;
 
+import com.epam.training.ticketservice.core.user.persistence.entity.UserEntity;
+import com.epam.training.ticketservice.core.user.persistence.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
 public class InMemoryDbInitializer {
+
+    private final UserRepository userRepository;
+
+    public InMemoryDbInitializer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        UserEntity admin = new UserEntity("admin", "admin", true);
+        userRepository.save(admin);
+    }
 }
