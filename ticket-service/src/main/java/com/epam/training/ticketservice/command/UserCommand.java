441e442562c84cc0d72a6787b2de005c874bd22f
raczandras <raczandras0204@gmail.com>
2021-05-03 19:16:55 +0200

    Made the usercommands actually work

--- ticket-service/src/main/java/com/epam/training/ticketservice/command/UserCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/command/UserCommand.java
@@ -1,27 +1,43 @@
 package com.epam.training.ticketservice.command;
 
+import com.epam.training.ticketservice.core.user.LoginService;
+import com.epam.training.ticketservice.core.user.exception.UserNotFoundException;
+import com.epam.training.ticketservice.core.user.model.UserDto;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
 public class UserCommand {
-    //TODO make commands actually work
 
-    public UserCommand() {
+    private final LoginService loginService;
+
+    public UserCommand(LoginService loginService) {
+        this.loginService = loginService;
     }
 
     @ShellMethod(value = "Login as admin", key = "sign in privileged")
     public String loginUser(String username, String password) {
-        return "logged in (maybe)";
+        try {
+            loginService.login(username, password);
+            UserDto user = loginService.getLoggedUser();
+            return "Signed in with " + user;
+        } catch (UserNotFoundException e) {
+            return e.getMessage();
+        }
     }
 
     @ShellMethod(value = "Describe current account", key = "describe account")
     public String describeUser() {
-        return "Signed in with: ";
+        try {
+            UserDto userDto = loginService.getLoggedUser();
+            return "Signed in with " + userDto;
+        } catch (UserNotFoundException e) {
+            return e.getMessage();
+        }
     }
 
     @ShellMethod(value = "Log out from current account", key = "sign out")
     public String logout() {
-        return "logged out";
+        return loginService.logout();
     }
 }
